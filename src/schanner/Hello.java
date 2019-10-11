f5572d9da12be7d85de0d32d0a5378a9898beec3
Adam <app18002@byui.edu>
2019-10-11 10:15:06 -0600

    Hello from Adam

--- src/schanner/Hello.java
+++ src/schanner/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Hannah");
         System.out.println("Hello Hannah, my name is Porter");
+        System.out.println("Hello Hannah, my name is Adam");
     }
 }
